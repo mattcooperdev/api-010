bacd95d48c7be509c92029e3e729d552b772cc0d
YesCoops <coopandmgc@gmail.com>
2022-03-10 14:37:42 +0000

    Update post Form function to send data to API

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,19 @@ const API_URL = "https://ci-jshint.herokuapp.com/api";
 const resutlsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
 
 document.getElementById("status").addEventListener("click", e => getStatus(e));
+document.getElementById("submit").addEventListener("click", e => postForm(e));
+
+async function postForm(e) {
+    const form = new FormData(document.getElementById("checksform"));
+
+    const response = await fetch(API_URL, {
+        method: "POST",
+        headers: {
+            "Authorization": API_KEY,
+        },
+        body: form,
+    });
+}
 
 async function getStatus(e) {
     const queryString = `${API_URL}?api_key=${API_KEY}`;
@@ -11,12 +24,12 @@ async function getStatus(e) {
 
     const data = await response.json();
 
-    if(response.ok) {
-        displayStatus(data); 
+    if (response.ok) {
+        displayStatus(data);
     } else {
-        throw new Error (data.error);
+        throw new Error(data.error);
     }
-} 
+}
 
 function displayStatus(data) {
 
@@ -28,5 +41,6 @@ function displayStatus(data) {
     document.getElementById('results-content').innerHTML = results;
 
     resutlsModal.show
-} 
+}
+
 
