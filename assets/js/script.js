55fd90b37fd9acce54998379ab257966889e84f1
YesCoops <coopandmgc@gmail.com>
2022-03-10 10:42:15 +0000

    Add AP info and Status function

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+const API_KEY = "wgkI78k5douF1LdXzJ3iKMr1yzU";
+const API_URL = "https://ci-jshint.herokuapp.com/api";
+const resutlsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
+
+document.getElementById("status").addEventListener("click", e => getStatus(e));
+
+async function getStatus(e) {
+    const queryString = `${API_URL}?api_key=${API_KEY}`;
+
+    const response = await fetch(queryString);
+
+    const data = await response.json();
+
+    if(response.ok) {
+        displayStatus(data); 
+    } else {
+        throw new Error (data.error);
+    }
+} 
+
+function displayStatus(data) {
+
+    let heading = "API Key Status";
+    let results = `<div>Your key is valid until</div>`;
+    results += `<div class="key-status">${data.expiry}</div>`
+
+    document.getElementById('resultsModalTitle').innerText = heading;
+    document.getElementById('results-content').innerHTML = results;
+
+    resutlsModal.show
+}
