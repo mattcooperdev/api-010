ec0e9a311451cae00ebf561ee13273cbf5798c0d
YesCoops <coopandmgc@gmail.com>
2022-03-11 14:51:34 +0000

    Update errors modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,7 @@ document.getElementById("status").addEventListener("click", e => getStatus(e));
 document.getElementById("submit").addEventListener("click", e => postForm(e));
 
 async function postForm(e) {
+
     const form = new FormData(document.getElementById("checksform"));
 
     const response = await fetch(API_URL, {
@@ -15,6 +16,14 @@ async function postForm(e) {
         },
         body: form,
     });
+
+    const data = await response.json();
+
+    if(response.ok) {
+        displayErrors(data);
+    } else {
+        throw new Error(data.error);
+    }
 }
 
 async function getStatus(e) {
@@ -31,16 +40,36 @@ async function getStatus(e) {
     }
 }
 
+function displayErrors(data) {
+
+    let results = "";
+
+    let heading = `JSHint results for ${data.file}`;
+    if (data.total_errors === 0) {
+        results = `<div class="no_errors">No errors reported!</div>`;
+    } else {
+        results = `<div>Total Errors: <span class="error_count">${data.total_errors}</span>`
+        for (let error of data.error_list) {
+            results += `<div>At line <span class="line">${error.line}</span>, `;
+            results += `column <span class="column">${error.col}:</span></div>`;
+            results += `<div class="error">${error.error}</div>`;
+        }
+    }
+
+    document.getElementById("resultsModalTitle").innerText = heading;
+    document.getElementById("results-content").innerHTML = results;
+    resutlsModal.show();
+} 
+
 function displayStatus(data) {
 
     let heading = "API Key Status";
     let results = `<div>Your key is valid until</div>`;
     results += `<div class="key-status">${data.expiry}</div>`
 
-    document.getElementById('resultsModalTitle').innerText = heading;
-    document.getElementById('results-content').innerHTML = results;
-
-    resutlsModal.show
+    document.getElementById("resultsModalTitle").innerText = heading;
+    document.getElementById("results-content").innerHTML = results;
+    resutlsModal.show();
 }
 
 
